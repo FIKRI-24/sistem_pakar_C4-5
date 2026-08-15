5e057ef18e41f8fa12b3b8eada05442173854f91
FIKRI-24 <irkif1011@gmail.com>
2026-08-15 17:54:52 +0700

    fix: force https scheme on production

--- karir-siswa/app/Providers/AppServiceProvider.php
+++ karir-siswa/app/Providers/AppServiceProvider.php
@@ -22,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFour();
+
+        if (config('app.env') === 'production' || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
